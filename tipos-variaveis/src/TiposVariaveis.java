03e9def074eb78e30325e5172b232dd1c92c8b68
Thaina Mendes <108478688+thainamends1@users.noreply.github.com>
2023-02-27 23:10:44 -0400

    Exemplos de variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;         //Se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L;    //Se começar com zero, talvez tenha que ser outro tipo
+        //float pi = 3.14F;
+        double salario = 1275.33;
+
+        final double VALOR_PI = 3.14;   //Variável que não pode ter seu valor alterado
     }
 }
