0bcb2bd2ba840d1927bb5815666ff91c100eb2bf
Thaina Mendes <108478688+thainamends1@users.noreply.github.com>
2023-02-28 20:55:33 -0400

    Criando a classe.

---
+++ operadores/operadores/src/TiposOperadores.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+    }
+}
