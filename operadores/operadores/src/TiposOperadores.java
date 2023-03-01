4986e1f7a4a33839031be2b05ac67faf7ef96f2e
Thaina Mendes <108478688+thainamends1@users.noreply.github.com>
2023-03-01 18:52:35 -0400

    Operando número e string.

--- operadores/operadores/src/TiposOperadores.java
+++ operadores/operadores/src/TiposOperadores.java
@@ -1,11 +1,33 @@
-public class App {
+public class TiposOperadores {
     public static void main(String[] args) throws Exception {
-        
+        /*
         double soma = 10.5 + 15.7;
         int subtracao = 113 - 25;
         int multiplicacao = 20 * 7;
         int divisao = 15 / 3;
         int modulo = 18 % 3;
         double resultado = (10 * 7) + (20 / 4);
+        */
+
+        //Soma de strings realiza a concatenação das strings
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
     }
 }
